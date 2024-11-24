f28932fff429602322c25b7ae465689a0646b0eb
cyantis <cyantis@gmail.com>
2024-11-23 17:27:42 -0700

    creates Order model

---
+++ Models/Order.cs
@@ -0,0 +1,19 @@
+public class Order
+{
+    public required string OrderId { get; set; }
+    public required List<Item> Items { get; set; }
+}
+
+public class Item
+{
+    public required string Description { get; set; }
+    public required string Status { get; set; }
+    public int DeliveryNotification { get; set; }
+
+    public bool IsDelivered => Status.Equals("Delivered", StringComparison.OrdinalIgnoreCase);
+
+    public void IncrementDeliveryNotification()
+    {
+        DeliveryNotification++;
+    }
+}
